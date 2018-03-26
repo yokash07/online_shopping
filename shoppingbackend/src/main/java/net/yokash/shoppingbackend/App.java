781bfac99f90e0599bc74c12fae1873cb0885c77
yokash07 <31814019+yokash07@users.noreply.github.com>
2018-03-26 23:53:26 +0530

    my project setup completed succesfully

---
+++ shoppingbackend/src/main/java/net/yokash/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.yokash.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
